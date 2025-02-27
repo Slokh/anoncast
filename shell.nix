44d5138527f892d51e3145106c74a9dedea154f0
Kumar Abhinav <97421185+0xabhinav@users.noreply.github.com>
2025-02-27 14:27:00 +0530

    Use correct branch

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+  # Pin to nixos-24.11 release
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz";
+  };
+  pkgs = import nixpkgs {};
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     bun
